ce665122547a12d7adaaf6d82894601516f1c7ce
Lyndel <lyndelt@gmail.com>
2011-05-28 17:00:45 +1000

    updated logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,14 @@
 var height;
 var width;
 var asset;
+var title;
+
+
+var littleContainer = '<div id="banner-container" class="hide"><p id="closer">Close X</p><div id="banner-preview"></div></div><div id="stage" class="hide"></div>';
+
 
 $(document).ready(function(){
-	 
+	 $('body').append(littleContainer);
 	 
 	 $('a[rel*=little_box]').click(function() {
 		var relContent = $(this).attr("rel");
@@ -13,17 +18,10 @@ $(document).ready(function(){
 		height = (heightParts[1]);
 		widthParts = substr[2].split('=');
 		width = (widthParts[1]);
-		
-		// alert('width:' + width + ' height: '+height);
-		
-		asset = $(this).html();
-		// alert (asset);
-		
-		
-		
+		asset = $(this).attr("href");
+		title = $(this).attr("title");
 		
 		showBanner();
-		
 		
 		return false;	 
 	 });
@@ -31,21 +29,20 @@ $(document).ready(function(){
 
 });
 
-
-
 function showBanner() {
 			var windowWidth = $(window).width();
 			var stageHeight = $(window).height() + $(window).scrollTop() ;
 			var posLeft = Math.max(windowWidth - width, 0) / 2 + $(window).scrollLeft();
 			var posTop = $(window).scrollTop() + 50;
+			var imageString = '<img src="' + asset + '" width="' + width + '" height="'+ height + '" title="' + title + '" />';
 			
-			$('#banner-preview').append(asset);
+			$('#banner-preview').append(imageString);
 			
 			
 			
-			$('#banner-container').css({'width': width, 'left' : posLeft, 'top' : posTop}).show();
+			$('#banner-container').css({'width': width, 'left' : posLeft, 'top' : posTop}).fadeIn('slow');
 			$('#stage').css({'height': stageHeight}).show();
-	// });
+	
 	
 	$('#closer').click(function() {
 			$('#banner-container').hide();
@@ -56,8 +53,3 @@ function showBanner() {
 			
 	
 	};
-
-
-
-
-
